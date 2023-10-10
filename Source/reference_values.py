3989fb815322a91a730dfc0b6957658c675708ff
nfeliccia <nic@secretsmokestack.com>
2023-10-10 11:43:14 -0400

    First commit with stuff.

---
+++ Source/reference_values.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+current_resume_path = Path(r"C:\Users\nfeli\OneDrive\2023_career\resumes\Feliccia_0x57_000044.docx")
+first_name = "Nicholas"
+last_name = "Feliccia"
+phone = "215-550-1642"
+email = "nic@secretsmokestack.com"
