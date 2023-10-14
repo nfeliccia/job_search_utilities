073e51cde6b65e6d080169d41ff22a799dd242ca
nfeliccia <nic@secretsmokestack.com>
2023-10-14 11:05:11 -0400

    Making Unit Tests

---
+++ Source/readers/sig_reader.py
@@ -0,0 +1,78 @@
+"""This script automates the process of searching for jobs at Susquehanna International Group.
+
+It uses Selenium WebDriver to navigate to the careers page of Susquehanna International Group,
+searches for jobs based on specified keywords, and optionally interacts with elements on the page,
+such as checkboxes for filtering job locations.
+
+Keywords for job searches are defined within the script, and the WebDriver will open a new browser
+tab for each keyword, perform the search, and interact with the page as defined in the script."""
+import urllib.parse
+
+from selenium import webdriver
+from selenium.common import NoSuchElementException, TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def susquehanna_international_reader(testmode=False):
+    """
+    Navigates to the careers page of Susquehanna International Group and performs job searches based on predefined keywords.
+
+    The function uses Selenium WebDriver to open a new browser window, perform job searches for each keyword,
+    and interacts with elements on the page (e.g., checking checkboxes for job location filters).
+    It performs these actions iteratively for each keyword defined within the function.
+
+    Args:
+        testmode: Auto close the window or not.
+    """
+
+    # Initialize the Chrome driver
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+
+    # Define the base URL and keywords
+    base_url = "https://careers.sig.com/search-results?keywords={}"
+    keywords = ['"Data Science"', '"Machine Learning"', '"Python"']
+
+    for keyword in keywords:
+        blank_ = "window.open('', '_blank');"
+        driver.execute_script(blank_)  # Open a new blank tab
+        handles_ = driver.window_handles[-1]
+        driver.switch_to.window(handles_)  # Switch to the newly opened tab
+        quote = urllib.parse.quote(keyword)
+        url_format = base_url.format(quote)
+        print(url_format)
+        driver.get(url_format)
+
+        # Wait for the page to load and check for the "Allow" button in the cookie popup, then click it
+        try:
+            element_xpath = '/html/body/div[1]/section[1]/div/div/div/div[2]/button'
+            locator = (By.XPATH, element_xpath)
+            element_located = EC.presence_of_element_located(locator=locator)
+            allow_button = WebDriverWait(driver, 2).until(element_located)
+            allow_button.click()
+
+        except (NoSuchElementException, TimeoutException) as _:
+            print(f"Cookie consent popup not found or handled for tab with keyword: {keyword}")
+
+        try:
+            # Add an explicit wait for the checkbox element to be visible
+            child__checkbox = "#JobLocationBody li:nth-child(1) .checkbox"
+            checkbox_element_locator = (By.CSS_SELECTOR, child__checkbox)
+            located = EC.visibility_of_element_located(checkbox_element_locator)
+            checkbox = WebDriverWait(driver, timeout=10).until(located)
+            checkbox.click()
+        except NoSuchElementException as _:
+            print(f"Checkbox not found in tab with keyword: {keyword} ")
+
+    if not testmode:
+        # Prompt the user to press Enter to keep the window open
+        input("Press Enter to close the browser...")
+
+    # Close the browser
+    driver.quit()
+
+
+if __name__ == '__main__':
+    susquehanna_international_reader(testmode=False)
---
+++ Source/readers/tandymtech_reader.py
@@ -0,0 +1,116 @@
+"""
+This script automates the process of navigating to two specific URLs using Selenium.
+For each URL, the script performs the following actions:
+1. Opens the URL in a Chrome browser.
+2. Waits for and clicks on the 'hs-eu-confirmation-button'.
+3. For the second URL, checks a specific checkbox if it's not already checked.
+4. Waits for the user to press Enter and then quits the browser.
+"""
+
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def configure_webdriver() -> webdriver.Chrome:
+    """
+    Configures and returns the Selenium webdriver for Chrome.
+    
+    Returns:
+        webdriver.Chrome: Configured Chrome webdriver instance.
+    """
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    return driver
+
+
+def click_confirmation_button(driver: webdriver.Chrome, eu_conf_button_xpath: str) -> None:
+    """
+    Waits for and clicks on the 'hs-eu-confirmation-button' using the provided webdriver.
+    
+    Args:
+        driver (webdriver.Chrome): The Chrome webdriver instance.
+        eu_conf_button_xpath (str): The XPATH for the 'hs-eu-confirmation-button'.
+    """
+    try:
+        located = EC.presence_of_element_located((By.XPATH, eu_conf_button_xpath))
+        button = WebDriverWait(driver, 10).until(located)
+        time.sleep(2)  # Give the button a moment to be clickable
+        button.click()
+    except Exception as e:
+        print(f"Error clicking hs-eu-confirmation-button: {e}")
+
+
+def open_url_in_new_tab(driver: webdriver.Chrome, url: str) -> None:
+    """
+    Opens a new browser tab and navigates to the specified URL.
+    
+    Args:
+        driver (webdriver.Chrome): The Chrome webdriver instance.
+        url (str): URL to navigate to.
+    """
+    driver.execute_script("window.open('', '_blank');")
+    driver.switch_to.window(driver.window_handles[-1])  # Switch to the last opened tab
+    driver.get(url)
+
+
+def tandym_tech_reader() -> None:
+    driver = configure_webdriver()
+
+    # Navigate to the first URL and click the confirmation button
+    pennsylvania = ("https://tandymtech.com/job-seekers/tech-search-results/?keyword=&where=19124,%20Philadelphia,"
+                    "%20Pennsylvania")
+    driver.get(pennsylvania)
+    time.sleep(3)
+    eu_button = '//*[@id="hs-eu-confirmation-button"]'
+    click_confirmation_button(driver, eu_button)
+
+    # Navigate to the second URL in a new tab and click the confirmation button
+    open_url_in_new_tab(driver, "https://tandymtech.com/job-seekers/tech-search-results/?keyword=(Remote)&where=")
+    click_confirmation_button(driver, eu_button)
+
+    scroll_to_bottom_of_section(driver, ".cards")
+    click_load_more_until_end(driver, "Load more")
+    input("Press Enter to quit...")
+
+
+def scroll_to_bottom_of_section(driver: webdriver.Chrome, css_selector: str) -> None:
+    """
+    Scrolls to the bottom of a specified section using its CSS selector.
+
+    Args:
+        driver (webdriver.Chrome): The Chrome webdriver instance.
+        css_selector (str): CSS selector of the section to scroll.
+    """
+    try:
+        element = driver.find_element_by_css_selector(css_selector)
+        driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight;", element)
+    except Exception as e:
+        print(f"Error scrolling to the bottom of the section: {e}")
+
+
+def click_load_more_until_end(driver: webdriver.Chrome, load_more_text: str) -> None:
+    """
+    Continuously clicks on an element with the specified text until it's no longer present or clickable.
+
+    Args:
+        driver (webdriver.Chrome): The Chrome webdriver instance.
+        load_more_text (str): Text of the element to click.
+    """
+    while True:
+        try:
+            load_more_button = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.XPATH, f"//*[contains(text(), '{load_more_text}')]"))
+            )
+            load_more_button.click()
+            time.sleep(2)  # Wait for the page to load new content
+        except Exception:
+            # If the button is not found or not clickable, break the loop
+            break
+
+
+if __name__ == "__main__":
+    tandym_tech_reader()
