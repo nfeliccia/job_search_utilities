44b87c09358202c876a99b49618616c590eea8f0
nfeliccia@hotmail.com <nic@secretsmokestack.com>
2023-12-07 13:10:46 -0500

    password leak fix.

--- Source/database_code/make_a_secret_driver.py
+++ Source/database_code/make_a_secret_driver.py
@@ -1,3 +1,3 @@
 from database_code.make_a_secret import store_secret
 
-store_secret(company_name="fedex", user_id="nic@secretsmokestack.com", password="!HeCof9Gh6evrq&F")
+store_secret(company_name="company_name", user_id="nic@secretsmokestack.com", password="abc_123_def_456")
